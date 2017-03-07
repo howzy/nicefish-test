e9dc78f7655c4d3a4706414816175676790e89a4
zh-once <1007552066@qq.com>
2017-03-07 16:45:20 +0800

    顶部导航条功能完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ElementRef, Renderer } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import './operators';
+
+import { UserLoginService } from './user/user-login/services/user-login.service';
+import { UserRegisterService } from './user/user-register/services/user-register.service';
+import { User } from './user/model/user-model';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +13,42 @@ import './operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app works!';
+  currentUser: User = <User>{};
+  globalClickCallbackFn: Function;
 
   constructor(
+    public router: Router,
     public activatedRoute: ActivatedRoute,
+    public renderer: Renderer,
+    public elementRef: ElementRef,
+    private userLoginService: UserLoginService,
+    private userRegisterService: UserRegisterService,
   ) { }
 
   ngOnInit() {
-    //
+    this.globalClickCallbackFn = this.renderer.listen(this.elementRef.nativeElement, 'click', event => {
+      console.log('全局监听点击事件>' + event);
+    });
+
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+    this.userLoginService.currentUser
+      .merge(this.userRegisterService.currentUser)
+      .subscribe(
+        data => {
+          this.currentUser = data;
+
+          // 如果是从/login这个URL进行的登录，跳转到首页，否则什么都不做
+          if (this.router.url.indexOf('/login') !== -1) {
+            this.router.navigateByUrl('home');
+          }
+        },
+        error => console.log(error)
+      );
+  }
+
+  doLogout(): void {
+    this.userLoginService.logout();
+    this.router.navigateByUrl('');
   }
 }
--- src/app/operators.ts
+++ src/app/operators.ts
@@ -1,2 +1,3 @@
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/merge';
