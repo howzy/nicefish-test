936896eb67a33953ce814a9cedf9c32d0806e697
zh-once <1007552066@qq.com>
2017-03-09 10:25:39 +0800

    评论管理组件完成

--- src/app/manage/comment-table/comment-table.component.ts
+++ src/app/manage/comment-table/comment-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, UrlTree, PRIMARY_OUTLET, UrlSegmentGroup, UrlSegment } from '@angular/router';
 
 @Component({
   selector: 'app-comment-table',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment-table.component.css']
 })
 export class CommentTableComponent implements OnInit {
+  maxSize: number = 5;
+  itemsPerPage: number = 5;
+  totalItems: number = 15;
+  currentPage: number = 1;
 
-  constructor() { }
+  constructor(private router: Router,
+    private activeRoute: ActivatedRoute) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.activeRoute.params.subscribe(
+      params => this.getCommentsByPage(params['page'])
+    );
+  }
+
+  getCommentsByPage(page: Number): void {
+    console.log('页码>' + page);
+  }
+
+  pageChanged(event: any): void {
+    const urlTree: UrlTree = this.router.parseUrl(this.router.url);
+    const g: UrlSegmentGroup = urlTree.root.children[PRIMARY_OUTLET];
+    const s: UrlSegment[] = g.segments;
+    this.router.navigateByUrl(s[0] + '/commenttable/page/' + event.page);
+  }
+
+  delComment(commentId: Number): void {
+    console.log(commentId);
+
+  }
 }
