df58d98680e6822ec083d2771feb5a5d69db57b0
zh-once <1007552066@qq.com>
2017-03-09 10:37:52 +0800

    用户管理组件完成

--- src/app/manage/user-table/user-table.component.ts
+++ src/app/manage/user-table/user-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-table',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-table.component.css']
 })
 export class UserTableComponent implements OnInit {
+  maxSize: number = 5;
+  itemsPerPage: number = 5;
+  totalItems: number = 15;
+  currentPage: number = 1;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router,
+    private activeRoute: ActivatedRoute) {
   }
 
+  ngOnInit() {
+    this.activeRoute.params.subscribe(
+      params => this.getUsersByPage(params['page'])
+    );
+  }
+
+  getUsersByPage(page: Number): void {
+    console.log('页码>' + page);
+  }
+
+  pageChanged(event): void {
+    this.router.navigateByUrl('manage/usertable/page/' + event.page);
+  }
+
+  newUser(): void {
+    this.router.navigateByUrl('manage/usertable/newuser');
+  }
+
+  blockUser(userId: Number): void {
+    console.log(userId);
+  }
+
+  unBlockUser(userId: Number): void {
+    console.log(userId);
+  }
+
+  resetPwd(userId: Number): void {
+    console.log(userId);
+  }
 }
