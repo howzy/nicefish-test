b6ae2a9bfc59f8ca860ae9fc6b4b441fe264735e
zh-once <1007552066@qq.com>
2017-03-09 15:34:21 +0800

    用户主页面添加文章管理、评论管理组件

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -2,6 +2,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { UserMainComponent } from './user-main/user-main.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { PostTableComponent } from '../manage/post-table/post-table.component';
+import { CommentTableComponent } from '../manage/comment-table/comment-table.component';
 import { AuthGuard } from '../user/user-login/services/auth-guard';
 
 const userRoutes: Routes = [
@@ -11,8 +13,25 @@ const userRoutes: Routes = [
     canActivate: [AuthGuard],
     children: [
       {
+        path: '',
+        redirectTo: 'posttable/page/1',
+        component: PostTableComponent
+      },
+      {
+        path: 'posttable/page/:page',
+        component: PostTableComponent
+      },
+      {
+        path: 'commenttable/page/:page',
+        component: CommentTableComponent
+      },
+      {
         path: 'profile',
         component: UserProfileComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'posttable/page/1'
       }
     ]
   }
